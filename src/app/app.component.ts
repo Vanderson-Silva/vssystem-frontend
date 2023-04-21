1fc966c1a8ad655014fb52fae4bdc4019a9b9d0c
wandersonwandao@hotmail.com <wandersonwandao@hotmail.com>
2023-04-21 00:42:44 -0300

    Criando o component cliente-List

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  template: 'app.Component.html'
+  templateUrl: 'app.Component.html'
 })
 export class AppComponent {
   title = 'vssystem-frontend';
