b009d6e5b16caf346bc2c0253a093822b08a3fa2
wandersonwandao@hotmail.com <wandersonwandao@hotmail.com>
2023-04-21 21:05:01 -0300

    Criando o menu Sidenav Lateral

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,20 @@
 import { NgModule } from "@angular/core";
 import { BrowserModule } from "@angular/platform-browser";
-
 import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app.component";
 import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 import { MatTableModule } from "@angular/material/table";
 import { MatFormFieldModule } from "@angular/material/form-field";
 import { MatPaginatorModule } from "@angular/material/paginator";
-import { MatSortModule } from "@angular/material/sort";
-
 import { SidenavComponent } from "./components/home/sidenav/sidenav.component";
 import { MatSidenavModule } from "@angular/material/sidenav";
-import { ClienteListComponent } from './components/cliente/cliente-list/cliente-list.component';
+import { ClienteListComponent } from "./components/cliente/cliente-list/cliente-list.component";
+import { MatToolbarModule } from "@angular/material/toolbar";
+import { MatIconModule } from "@angular/material/icon";
+import { MatMenuModule } from "@angular/material/menu";
+import { MatButtonModule } from "@angular/material/button";
+import { MatListModule } from '@angular/material/list';
+
 
 @NgModule({
   declarations: [AppComponent, SidenavComponent, ClienteListComponent],
@@ -22,8 +25,13 @@ import { ClienteListComponent } from './components/cliente/cliente-list/cliente-
     MatTableModule,
     MatFormFieldModule,
     MatPaginatorModule,
-    MatSortModule,
+    MatMenuModule,
     MatSidenavModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatButtonModule,
+    MatListModule 
+    
   ],
   providers: [],
   bootstrap: [AppComponent],
--- src/app/components/home/sidenav/sidenav.component.ts
+++ src/app/components/home/sidenav/sidenav.component.ts
@@ -1,14 +1,25 @@
 import { Component, OnInit } from "@angular/core";
 
+
 @Component({
   selector: "app-sidenav",
   templateUrl: "./sidenav.component.html",
   styleUrls: ["./sidenav.component.css"],
 })
+
+
+
 export class SidenavComponent implements OnInit {
-  showFiller = false;
+
 
   ngOnInit(): void {
-    throw new Error("Method not implemented.");
+    
+    
   }
+
+
+  
 }
+
+
+
