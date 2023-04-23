687c88d1fe4868bb8b0e313b2eb0eaf8be28dd55
wandersonwandao@hotmail.com <wandersonwandao@hotmail.com>
2023-04-23 20:59:34 -0300

    Criando o Arquivo e a Interface models cliente.ts

---
+++ src/app/models/cliente.ts
@@ -0,0 +1,13 @@
+export interface Cliente {
+  id?: string;
+  nome: string;
+  dataNascimento: Date;
+  dataCadastro: Date;
+  status: string;
+  endereco: string;
+  numero: string;
+  bairro: string;
+  cidade: string;
+  telefone: string;
+  celular: string;
+}
