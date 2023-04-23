297a1f52f022164795310c0c86395370584d1746
wandersonwandao@hotmail.com <wandersonwandao@hotmail.com>
2023-04-22 23:33:06 -0300

    Criando a Rota para Listar todos os Clientes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from "@angular/core";
 import { RouterModule, Routes } from "@angular/router";
 import { SidenavComponent } from "./components/sidenav/sidenav.component";
 import { HomeComponent } from "./components/home/home.component";
+import { ClienteListComponent } from "./components/cliente/cliente-list/cliente-list.component";
 
 const routes: Routes = [
   {
@@ -11,6 +12,10 @@ const routes: Routes = [
       {
         path: "home",
         component: HomeComponent,
+      },
+      {
+        path: "listar-clientes",
+        component: ClienteListComponent,
       },
     ],
   },
--- src/app/components/cliente/cliente-list/cliente-list.component.ts
+++ src/app/components/cliente/cliente-list/cliente-list.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
+import { MatTableDataSource } from "@angular/material/table";
+
+export interface User {
+  name: string;
+  email: string;
+  phone: string;
+}
+
+const USERS: User[] = [
+  { name: "John", email: "john@example.com", phone: "123-456-7890" },
+  { name: "Jane", email: "jane@example.com", phone: "987-654-3210" },
+  { name: "Bob", email: "bob@example.com", phone: "555-555-5555" },
+];
 
 @Component({
-  selector: 'app-cliente-list',
-  templateUrl: './cliente-list.component.html',
-  styleUrls: ['./cliente-list.component.css']
+  selector: "app-cliente-list",
+  templateUrl: "./cliente-list.component.html",
+  styleUrls: ["./cliente-list.component.css"],
 })
 export class ClienteListComponent {
-
+  displayedColumns: string[] = ["name", "email", "phone"];
+  dataSource = new MatTableDataSource<User>(USERS);
 }
