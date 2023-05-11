22c6d2751a6a01336e714d457c17c0672d3c952b
wandersonwandao@hotmail.com <wandersonwandao@hotmail.com>
2023-05-10 22:50:34 -0300

    Criando a rota para cliente-update

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,6 +4,7 @@ import { SidenavComponent } from "./components/sidenav/sidenav.component";
 import { HomeComponent } from "./components/home/home.component";
 import { ClienteListComponent } from "./components/cliente/cliente-list/cliente-list.component";
 import { ClienteCreateComponent } from "./components/cliente/cliente-create/cliente-create.component";
+import { ClienteUpdateComponent } from "./components/cliente/cliente-update/cliente-update.component";
 
 const routes: Routes = [
   {
@@ -13,6 +14,7 @@ const routes: Routes = [
       { path: "home", component: HomeComponent },
       { path: "clientes-list", component: ClienteListComponent },
       { path: "clientes-create", component: ClienteCreateComponent },
+      { path: "clientes-update/:id", component: ClienteUpdateComponent },
     ],
   },
 ];
